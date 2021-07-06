64870ff1dc9065a247d1380c8e8eca1d417c3534
huxiaodi-sl <huxiaodi@sailing3d.cn>
2021-07-06 12:10:45 +0800

    support node

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# CHANGELOG
+
+## 2021-07-06 @huxiaodi-sl
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-export {orient2d, orient2dfast} from './esm/orient2d.js';
-export {orient3d, orient3dfast} from './esm/orient3d.js';
-export {incircle, incirclefast} from './esm/incircle.js';
-export {insphere, inspherefast} from './esm/insphere.js';
+module.exports = {
+  ...require("./umd/orient2d"),
+  ...require("./umd/orient3d"),
+  ...require("./umd/incircle"),
+  ...require("./umd/insphere"),
+};
